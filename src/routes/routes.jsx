beb75543df808258e1ebaf66b1c91afa8bc32420
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-04-02 18:05:28 +0100

    routes: add login page route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/home";
 import NewPostPage from "../pages/NewPostPage";
 import PostPage from "../pages/PostPage";
 import ErrorPage from "../pages/ErrorElement";
+import Login from "../pages/Login"
 
 
 const routes = [
@@ -13,7 +14,8 @@ const routes = [
         children: [
             { index: true, element: <Home /> },
             { path: "/newpost", element: <NewPostPage /> },
-            { path: "/post", element: <PostPage /> }
+            { path: "/post", element: <PostPage /> },
+            { path: "/login", element: <Login /> }
         ]
     },
 ];
