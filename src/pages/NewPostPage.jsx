a32b5f01609cf9e4e732ece2dbdbeccec89ff527
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2026-04-15 12:11:23 +0100

    feat: create new post successfully

--- src/pages/NewPostPage.jsx
+++ src/pages/NewPostPage.jsx
@@ -1,14 +1,49 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
 
 const NewPostPage = () => {
     const editorRef = useRef(null);
+    const [loading, setLoading] = useState(false)
 
-    const log = () => {
+
+    const handleCreateNewPost = async (e) => {
+        e.preventDefault()
+
+        const accessToken = sessionStorage.getItem('accessToken')
+        if (!accessToken) return;
+
+        const formData = new FormData(e.target)
+
         if (editorRef.current) {
-            console.log(editorRef.current.getContent());
+            formData.append('text', editorRef.current.getContent({ format: 'text' }));
         }
+
+        try {
+            setLoading(true)
+            const response = await fetch('/api/posts', {
+                method: 'POST',
+                headers: {
+                    "Authorization": `Bearer ${accessToken}`
+                },
+                body: formData
+            })
+
+            if (!response.ok) {
+                console.log(response)
+                throw new Error('Failed to create post')
+            }
+
+            const data = await response.json()
+            // console.log(data)
+
+        } catch (err) {
+            console.error(err)
+        } finally {
+            setLoading(false)
+        }
+
+
     };
 
     const myApiKey = `${import.meta.env.VITE_TINYMCE_API_KEY}`
@@ -20,11 +55,13 @@ const NewPostPage = () => {
                 className='flex-1 flex'
             >
                 <form
-                    className='flex-1 grid grid-cols-[60%_1fr] grid-rows-[20%_1fr_20%]'
+                    onSubmit={handleCreateNewPost}
+                    className='flex-1 grid grid-cols-[60%_1fr] grid-rows-[20%_1fr_20%] bigDiv'
                 >
                     <p className='flex flex-col gap-2 p-2'>
                         <label className='text-lg font-semibold' htmlFor="title">Title</label>
                         <input
+                            required
                             className='outline-none border border-brdClr h-13 w-[70%] pl-2.5 rounded-sm focus:border-primary'
                             type="text"
                             id="title"
@@ -32,37 +69,32 @@ const NewPostPage = () => {
                         />
                     </p>
 
-                    <div className='row-span-2'>
-                        <label htmlFor="isPublished" className='text-lg font-semibold isPublishedLabel'>
-                            Do you want to publish this immediately ?
-                            <p>
-                                <input
-                                    type="radio"
-                                    id="yes"
-                                    name='isPublished'
-                                    value={true} />
-                                <label htmlFor="yes">Yes</label>
-                            </p>
+                    <div className='row-span-2 flex flex-col justify-center items-center'>
 
-                            <p>
-                                <input
-                                    type="radio"
-                                    id="no"
-                                    name='isPublished'
-                                    value={false}
-                                />
-                                <label htmlFor="no">No</label>
-                            </p>
-                        </label>
+                        <fieldset className='flex flex-col gap-2 text-lg font-semibold'>
+                            <legend className='mb-6'>Publish now?</legend>
+
+                            <label>
+                                <input className='mr-1' type="radio" name="published" value="true" required />
+                                Yes
+                            </label>
+
+                            <label>
+                                <input className='mr-1' type="radio" name="published" value="false" />
+                                No
+                            </label>
+                        </fieldset>
 
                         <label className="mt-6 ml-4 inline-block cursor-pointer bg-primary text-white px-4 py-2 rounded-md hover:bg-darkerPrimary transition">
-                            Choose File
-                            <input type="file" className="hidden" />
+                            <input type="file" name='image' required />
                         </label>
+
                     </div>
 
                     <p className='row-span-2'>
                         <Editor
+                            ref={editorRef}
+                            onInit={(_evt, editor) => editorRef.current = editor}
                             apiKey={myApiKey}
                             init={{
                                 plugins: [
@@ -90,11 +122,13 @@ const NewPostPage = () => {
                     </p>
 
 
-                    <p className='flex items-start '>
+                    <p className='flex justify-center items-start'>
                         <button
-                            onClick={(e) => e.preventDefault()}
+                            disabled={loading}
                             className='createPostBtn w-20 ml-4'
-                        >Save</button>
+                        >
+                            {loading ? 'Saving ..' : 'Save'}
+                        </button>
                     </p>
                 </form>
             </div>
